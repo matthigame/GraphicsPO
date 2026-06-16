e0fddcb59f23fef05d9d1233dce5ee4bd1b0d8d2
matthigame <matthiasnijman08@gmail.com>
2026-06-16 21:00:51 +0200

    final things

--- obj/Debug/net10.0/INFOGRTemplate.AssemblyInfo.cs
+++ obj/Debug/net10.0/INFOGRTemplate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INFOGRTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8292ce450a688e12dcf96ee0d184a4df6e54a3e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+420a174b1c7d83141fc124b0d0053c2b1354790b")]
 [assembly: System.Reflection.AssemblyProductAttribute("INFOGRTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INFOGRTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
